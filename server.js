614f7ae4e7418c668d6b3d32d611b13d4e0d6ebc
Brad Stell <bradstell87@gmail.com>
2017-04-23 00:35:34 -0400

    Getting ready for heroku

--- server.js
+++ server.js
@@ -3,12 +3,20 @@ const app = express();
 const twilio = require('twilio');
 const bodyParser = require('body-parser');
 const NLU_V1 = require('watson-developer-cloud/natural-language-understanding/v1.js');
+const ConversationV1 = require('watson-developer-cloud/conversation/v1');
+let lastConvoContext;
 
 const vans = {
     'old-school': 'https://www.dropbox.com/s/mpldv80yjh5faw8/vans_oldschool.jpg?raw=1',
     'normal': 'https://www.dropbox.com/s/ih6tuz6x0l62bye/vans-era-mens.jpg?raw=1',
     'high-top': 'https://www.dropbox.com/s/lq3rs3ja0kowfqa/vans-hi-top.jpg?raw=1'
 }
+
+const conversation = new ConversationV1({
+    username: 'ff71802a-62b2-47ae-9692-df5e71c142b9',
+    password: 'fRohP03kApiD',
+    version_date: ConversationV1.VERSION_DATE_2017_02_03
+});
 
 let nlp = new NLU_V1({
     'username': 'e102f18e-7997-468e-9b9a-a9ca61fdcc71',
@@ -59,11 +67,17 @@ app.post('/receive', (req, res) => {
             console.log(JSON.stringify(response, null, 2));
             
             let sentiment = (response && response.sentiment && response.sentiment.document) ? response.sentiment.document.label : '';
+            let subject = (response.keywords.length > 0) ? response.keywords[0].text : '';
 
-            if (sentiment !== 'negative') {
-                let subject = (response.keywords.length > 0) ? response.keywords[0].text : '';
+            let one = message.toLowerCase().indexOf('upset');
+            let one_ = ~message.toLowerCase().indexOf('upset');
+            let two = message.toLowerCase().indexOf('disappointed');
+            let two_ = ~message.toLowerCase().indexOf('disappointed');
 
-                if (~subject.toLowerCase().indexOf('vans') && (response.semantic_roles.length > 0 && response.semantic_roles[0].action.verb.text === 'need')) {
+            if (sentiment !== 'negative' || !(~message.toLowerCase().indexOf('upset') || ~message.toLowerCase().indexOf('disappointed'))) {
+                
+
+                if (~subject.toLowerCase().indexOf('vans')/* && (response.semantic_roles.length > 0 && (response.semantic_roles[0].action.verb.text === 'need' || response.semantic_roles[0].action.verb.text === 'want'))*/) {
                     
                     twiml.message(function() {
                         this.body('Ok, Do you like any of these?');
@@ -75,6 +89,48 @@ app.post('/receive', (req, res) => {
                     res.end(twiml.toString());
 
                     return;
+                } else { // all other convo will be routed to Watson's Conversation API
+
+                    if (lastConvoContext) {
+                        conversation.message({
+                                input: { text: message },
+                                workspace_id: '267b330b-e345-4e6d-9eb8-1962deba1999',
+                                context: lastConvoContext
+                            }, function (err, response) {
+                                if (err) console.error(err);
+
+                                else {
+                                    twiml.message(response.output.text[0]);
+                                    res.writeHead(200, {'Content-Type': 'text/xml'});
+                                    res.end(twiml.toString());
+                                }
+                        });
+                    } else {
+                        conversation.message({
+                                input: { text: message },
+                                workspace_id: '267b330b-e345-4e6d-9eb8-1962deba1999'                            
+                            }, function(err, response) {
+                                if (err) console.error(err);
+
+                                else {
+                                    console.log(JSON.stringify(response, null, 2));
+                                    conversation.message({
+                                            input: { text: message },
+                                            workspace_id: '267b330b-e345-4e6d-9eb8-1962deba1999',
+                                            context: response.context
+                                        }, function(err, response) {
+                                            if (err) console.error(err);
+                                            else {
+                                                lastConvoContext = response.context;
+
+                                                twiml.message(response.output.text[0]);
+                                                res.writeHead(200, {'Content-Type': 'text/xml'});
+                                                res.end(twiml.toString());
+                                            }
+                                    });
+                                }
+                        });
+                    }
                 }
             } else {
                 
